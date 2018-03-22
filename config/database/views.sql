ae8dccad3ec6354be11b1a1012d709cbb950460a
dhehli <dh@netlive.ch>
2018-03-22 10:53:31 +0100

    fix view bug

--- config/database/views.sql
+++ config/database/views.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW v_user AS SELECT * FROM user WHERE deleted = 0;
 
-CREATE OR REPLACE VIEW v_onlinestate AS SELECT * FROM user WHERE deleted = 0;
+CREATE OR REPLACE VIEW v_onlinestate AS SELECT * FROM onlinestate WHERE deleted = 0;
 
-CREATE OR REPLACE VIEW v_userpermission AS SELECT * FROM user WHERE deleted = 0;
+CREATE OR REPLACE VIEW v_userpermission AS SELECT * FROM userpermission WHERE deleted = 0;
 
-CREATE OR REPLACE VIEW v_userstate AS SELECT * FROM user WHERE deleted = 0;
+CREATE OR REPLACE VIEW v_userstate AS SELECT * FROM v_userstate WHERE userstate = 0;
 
 CREATE OR REPLACE VIEW v_user_forgotpassword AS SELECT * FROM user_forgotpassword WHERE deleted = 0;
